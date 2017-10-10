3440a0495ae5a28485325d0ba5993816b5672024
Krisnamourt da Silva Correia Filho <krisnamourt_ti@hotmail.com>
2017-10-10 11:20:17 -0300

    initiate postgresql
    initiate postgresql

---
+++ PostgreSQL/calculate-tables-size.sql
@@ -0,0 +1,10 @@
+/* To find tables size
+
+@author Krisnamourt Filho - krisnamourt_ti@hotmail.com
+*/
+
+SELECT
+  relname as "Table",
+  pg_size_pretty(pg_total_relation_size(relid)) As "Size",
+  pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as "External Size"
+  FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;
---
+++ PostgreSQL/find-duplicate-keys.sql
@@ -0,0 +1,9 @@
+/*Query to find out duplicate key in a table
+
+@author Krisnamourt Filho - krisnamourt_ti@hotmail.com
+*/
+
+select <column_key1> ,<column_key2>,count(*)
+from <Search_Table>
+	group by <column_key1> ,<column_key2>
+	having count(*) >1
